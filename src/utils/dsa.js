b6df67aea6afdaccabe2da032ac157607c8c833d
Akash Sharma <akash_18@192.168.1.25>
2026-05-27 23:17:55 +0530

    feat

--- src/utils/dsa.js
+++ src/utils/dsa.js
@@ -41,3 +41,23 @@ setTimeout(()=>{
   cancel()
   console.log("hello interval")
 }, 2000)
+
+
+// Function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+  
+  return prefix;
+}
+
+const strings = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strings)); // Output: "fl"
