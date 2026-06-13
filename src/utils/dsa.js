df897002dc52862225e0977d7e0ac25d7d83db7c
Akash Sharma <akash_18@192.168.1.8>
2026-06-13 23:46:20 +0530

    fix

--- src/utils/dsa.js
+++ src/utils/dsa.js
@@ -118,3 +118,28 @@ function flattenArray(arr) {
 
 const nestedArray = [1, [2, [3, 4], 5], 6];
 console.log(flattenArray(nestedArray)); // Output: [1, 2, 3, 4, 5, 6]
+
+
+// Container With Most Water
+function maxArea(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    const width = right - left;
+    const currentArea = Math.min(height[left], height[right]) * width;
+    maxArea = Math.max(maxArea, currentArea);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+}
+
+const heights = [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(heights)); // Output: 49
